d89345406d41d128954920726cfb00a629c86741
josephyv <josephynimoto@gmail.com>
2023-05-19 10:50:52 -0600

    things

--- settings/screens.py
+++ settings/screens.py
@@ -39,12 +39,15 @@ if  not command.returncode:
     connected_screens = int(command.stdout.decode("UTF-8"))
 
 if connected_screens > 1:
-    for _ in range (1, connected_screens):
-        screens.append(Screen(
-                wallpaper=today_wall(),
-                wallpaper_mode="stretch",
-                top=gen_bar(SECONDARY_WIDGETS, 22)))
-
     screen_config = "xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1366x768 --pos 0x0 --rotate normal"
-    subprocess.run(screen_config, shell=True)
-
+    screen_command = subprocess.Popen(screen_config, shell=True, stdout=subprocess.PIPE)
+    if not command.returncode:
+        for _ in range (1, connected_screens):
+            screens.append(Screen(
+                    wallpaper=today_wall(),
+                    wallpaper_mode="stretch",
+                    top=gen_bar(SECONDARY_WIDGETS, 22)))
+else:
+    laptop = 'xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off'
+    subprocess.run(laptop, shell=True, stdout=subprocess.PIPE)
+    
