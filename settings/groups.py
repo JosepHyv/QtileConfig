31cd1d124ec1b545e70764e666035904ab7b8605
josephyv <haguilar@auronix.com>
2023-11-19 02:34:32 -0600

    reconfigure keys

--- settings/groups.py
+++ settings/groups.py
@@ -5,53 +5,65 @@ from libqtile.lazy import lazy
 
 # unicode for nerd unifonts https://www.nerdfonts.com/cheat-sheet
 NAMES = [
-    '', # 'Web', nf-fa-chrome
-    '', # 'Term', nf-oct-terminal
-    '', # 'Dev', nf-dev-code
-    '', # 'Auronix', nf-linux-archlinux
-    '', # 'DBas', nf-fa-database
+    "",  # 'Web', nf-fa-chrome
+    "",  # 'Dev', nf-dev-code
+    "",  # 'Term', nf-oct-terminal
+    "",  # 'Auronix', nf-linux-archlinux
+    "",  # 'DBas', nf-fa-database
     '󰑴', # 'Esc', nf-md-school
-    '', # 'Spotify', nf-fa-spotify
+    "",  # 'Spotify', nf-fa-spotify
 ]
 
-browsers = ['Chromium', 'google-chrome', 'firefox']
+browsers = ["Chromium", "google-chrome", "firefox"]
 
-groups = [Group(
-        NAMES[0], 
-        matches=[Match(wm_class=name) for name in browsers]
-    )] + [Group(current) for current in NAMES[1::]]
+groups = (
+    [Group(NAMES[0], matches=[Match(wm_class=name) for name in browsers])]
+    + [Group(NAMES[1], matches=[Match(wm_class="code")])]
+    + [Group(current) for current in NAMES[2::]]
+)
 
-def group_position(name : str ) -> str:
+
+def group_position(name: str) -> str:
     global NAMES
     lower_names = [x.lower() for x in NAMES]
-    position_ans = "" 
+    position_ans = ""
     if name.lower() in lower_names:
-        position_ans = str(lower_names.index(name.lower()) + 1 )
+        position_ans = str(lower_names.index(name.lower()) + 1)
     return position_ans
+
 
 for current in groups:
     keys.extend(
-                [
-                    Key(
-                        [MOD], 
-                        group_position(current.name), 
-                        lazy.group[current.name].toscreen(),
-                        desc="hack para cambiar al grupo dado por posiciones entre 1 y len(grupos)"
-                    ),
-                    Key(
-                        [MOD, "shift"], 
-                        group_position(current.name), 
-                        lazy.window.togroup(current.name, switch_group=True),
-                        desc="hack para mover una ventana al grupo en el rango de 1 y len(grupos)"
-                    )
-                ]
-            )
+        [
+            Key(
+                [MOD],
+                group_position(current.name),
+                lazy.group[current.name].toscreen(),
+                desc="hack para cambiar al grupo dado por posiciones entre 1 y len(grupos)",
+            ),
+            Key(
+                [MOD, "shift"],
+                group_position(current.name),
+                lazy.window.togroup(current.name, switch_group=True),
+                desc="hack para mover una ventana al grupo en el rango de 1 y len(grupos)",
+            ),
+        ]
+    )
 
-keys.extend([
-    Key([MOD], 'Right', lazy.screen.next_group(), desc='cambiando al siguiente grupo a la derecha'),
-    Key([MOD], 'Left', lazy.screen.prev_group(), desc='cambiando al  grupo anterior a la izquierda'),
- ])
+keys.extend(
+    [
+        Key(
+            [MOD],
+            "Right",
+            lazy.screen.next_group(),
+            desc="cambiando al siguiente grupo a la derecha",
+        ),
+        Key(
+            [MOD],
+            "Left",
+            lazy.screen.prev_group(),
+            desc="cambiando al  grupo anterior a la izquierda",
+        ),
+    ]
+)
 
-
-
- 
--- settings/keys.py
+++ settings/keys.py
@@ -1,8 +1,8 @@
 import os
-from libqtile.config import Key 
-from libqtile.lazy import lazy 
+from libqtile.config import Key
+from libqtile.lazy import lazy
 
-# Mi config 
+# Mi config
 
 
 MOD = "mod4"
@@ -10,81 +10,104 @@ Alt = "mod1"
 TERMINAL = "kitty"
 
 keys = [
-    #cambio entre ventanas (solo el foco de ventanas)
-    Key([MOD], "h", lazy.layout.left(), desc="Move focus to left"),
+    # cambio entre ventanas (solo el foco de ventanas)
+    Key([MOD], "j", lazy.layout.left(), desc="Move focus to left"),
     Key([MOD], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([MOD], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([MOD], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([MOD], "i", lazy.layout.grow()),
-    Key([MOD], "m", lazy.layout.shrink()),
-    Key([MOD], "n", lazy.layout.normalize()),
-    Key([MOD], "o", lazy.layout.maximize()),
+    Key([MOD], "k", lazy.layout.down(), desc="Move focus down"),
+    Key([MOD], "i", lazy.layout.up(), desc="Move focus up"),
     Key([MOD], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key([MOD, "shift"], "space", lazy.layout.flip()),
-
-    # control de columnas y filas 
-    Key([MOD, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([MOD, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([MOD, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([MOD, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([MOD, Alt], "j", lazy.layout.flip_down()),
-    Key([MOD, Alt], "k", lazy.layout.flip_up()),
-    Key([MOD, Alt], "h", lazy.layout.flip_left()),
+    Key(
+        [MOD],
+        "f",
+        lazy.window.toggle_fullscreen(),
+        desc="Full Screen in Window",
+    ),
+    Key(
+        [MOD],
+        "b",
+        lazy.hide_show_bar(),
+        desc="Full Screen in Window",
+    ),
+    Key(
+        [MOD, "shift"],
+        "j",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left",
+    ),
+    Key(
+        [MOD, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
+    Key([MOD, "shift"], "k", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([MOD, "shift"], "i", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([MOD, Alt], "k", lazy.layout.flip_down()),
+    Key([MOD, Alt], "i", lazy.layout.flip_up()),
+    Key([MOD, Alt], "j", lazy.layout.flip_left()),
     Key([MOD, Alt], "l", lazy.layout.flip_right()),
-    Key([MOD, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([MOD, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([MOD, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([MOD, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key(
+        [MOD, "control"],
+        "l",
+        lazy.layout.grow_right(),
+        desc="Grow window to the right",
+    ),
+    Key(
+        [MOD, "control"],
+        "j",
+        lazy.layout.grow_left(),
+        desc="Grow window to the right",
+    ),
+    Key([MOD, "control"], "k", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([MOD, "control"], "i", lazy.layout.grow_up(), desc="Grow window up"),
     Key([MOD], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    
-
-    #redimencionar las ventanas 
-    Key([MOD], "Return", lazy.spawn(TERMINAL), desc="abre instancia de kitty terminal"),
-    # Toggle between different layouts as defined below
-    Key([MOD], "Tab", lazy.next_layout(), desc="alterna las ventanas (maximizadas)"),
     Key([MOD], "w", lazy.window.kill(), desc="Mata ventana actual"),
+    Key([MOD], "Tab", lazy.next_layout(), desc="alterna las ventanas (maximizadas)"),
     Key([MOD, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([MOD], "q", lazy.spawn(os.path.expanduser('~/.config/rofi/powermenu.sh'))),
+    Key([MOD], "q", lazy.spawn(os.path.expanduser("~/.config/rofi/powermenu.sh"))),
     Key([MOD, "control", Alt], "l", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([Alt], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
-
-    # Configuracion de rofi 
-
-    Key([Alt], "space", lazy.spawn(os.path.expanduser('~/.config/rofi/rofi-run.sh'))),
-    Key([Alt], "Tab", lazy.spawn("rofi -show window")),
-    Key([MOD, Alt], "space", lazy.spawncmd()),
-
-    #Control de volumne 
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")),
+    # Control de volumne
+    Key(
+        [],
+        "XF86AudioLowerVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+    ),
+    Key(
+        [],
+        "XF86AudioRaiseVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+    ),
     Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
     Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
     Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
-
-    ## capturar pantalla 
-     Key([], "Print", lazy.spawn("spectacle -s -b --fullscreen --copy-image")),
-     Key([MOD], "Print", lazy.spawn("spectacle -s -b --fullscreen")),
-     Key([Alt], "Print", lazy.spawn("spectacle -s -b -w --region --copy-image")),
-     Key([MOD, Alt], "Print", lazy.spawn("spectacle -s -b --region")),      
-     Key(['control'], "Print", lazy.spawn("spectacle -s -b --activewindow --copy-image")),       
-     Key([MOD, 'control'], "Print", lazy.spawn("spectacle -s -b --activewindow")),       
-     Key([MOD, Alt], "l", lazy.spawn("light-locker-command --lock")),
-
-     ## ventanas flotantes 
-     Key([MOD, "shift"], 'f', lazy.window.toggle_floating()),
-     Key([MOD, "shift"], "s",lazy.layout.toggle_split()),
-     Key([MOD], "p", lazy.widget["keyboardlayout"].next_keyboard()),
-
-     ## My main apps workflow
-     Key([MOD], "m", lazy.spawn('min --force-device-scale-factor=0.89')),
-     Key([MOD], 'e', lazy.spawn('thunar')),
-     Key([MOD], 't', lazy.spawn('com.todoist.Todoist')), 
-     Key([MOD], 's', lazy.spawn('slack')),  
-     Key([MOD], 'g', lazy.spawn('google-chrome-stable --force-device-scale-factor=0.89')),  
-     Key([MOD], "x", lazy.spawn('chromium --force-device-scale-factor=0.89')),
-     Key([MOD], 'c', lazy.spawn('code --new-window')),
+    ## capturar pantalla
+    Key([], "Print", lazy.spawn("spectacle -s -b --fullscreen --copy-image")),
+    Key([MOD], "Print", lazy.spawn("spectacle -s -b --fullscreen")),
+    Key([Alt], "Print", lazy.spawn("spectacle -s -b -w --region --copy-image")),
+    Key([MOD, Alt], "Print", lazy.spawn("spectacle -s -b --region")),
+    Key(
+        ["control"], "Print", lazy.spawn("spectacle -s -b --activewindow --copy-image")
+    ),
+    Key([MOD, "control"], "Print", lazy.spawn("spectacle -s -b --activewindow")),
+    # Key([MOD, Alt], "l", lazy.spawn("light-locker-command --lock")),
+    ## ventanas flotantes
+    Key([MOD, "shift"], "f", lazy.window.toggle_floating()),
+    Key([MOD, "shift"], "s", lazy.layout.toggle_split()),
+    Key([MOD], "p", lazy.widget["keyboardlayout"].next_keyboard()),
+    ## My main apps workflow
+    # Configuracion de rofi
+    Key([Alt], "space", lazy.spawn(os.path.expanduser("~/.config/rofi/rofi-run.sh"))),
+    Key([Alt], "Tab", lazy.spawn("rofi -show window")),
+    Key([MOD], "Return", lazy.spawn(TERMINAL), desc="abre instancia de kitty terminal"),
+    Key([MOD], "m", lazy.spawn("min --force-device-scale-factor=0.89")),
+    Key([MOD], "e", lazy.spawn("thunar")),
+    Key([MOD], "t", lazy.spawn("com.todoist.Todoist")),
+    Key([MOD], "s", lazy.spawn("slack")),
+    Key(
+        [MOD], "g", lazy.spawn("google-chrome-stable --force-device-scale-factor=0.89")
+    ),
+    Key([MOD], "x", lazy.spawn("chromium --force-device-scale-factor=0.89")),
+    Key([MOD], "c", lazy.spawn("code --new-window")),
 ]
-
-
 
